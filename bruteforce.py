a0b415637d868cd0a8e0c3c5b956d8185f43e7e5
Jonny Saykosy <josayko@pm.me>
2021-12-26 17:24:27 +0100

    Update recursive bruteforce with comment + fix total profit calculation

--- bruteforce.py
+++ bruteforce.py
@@ -5,6 +5,9 @@ import sys
 
 
 def serialize_data_from_csv(filepath):
+    """Serialize data as -> dataset = [(name, price, profit)].
+    Profit is the profit ratio value of the price -> float(data[1]) * float(data[2]) / 100)
+    """
     with open(filepath, "r") as file:
         # Split lines into an array
         lines = file.read().splitlines()
@@ -13,32 +16,47 @@ def serialize_data_from_csv(filepath):
     dataset = []
     for line in lines:
         data = line.split(',')
-        dataset.append((data[0], float(data[1]), float(data[2])))
+        dataset.append((data[0], float(data[1]), float(data[1]) * float(data[2]) / 100))
     return dataset
 
 
 def calculate_profit(results):
-    portfolio = results[1]
-    total = 0
-    for stock in portfolio:
-        profit = stock[1] * stock[2] / 100
-        print(f"{stock[0]:{' '}<{10}} {stock[1]:{' '}<{10}} {stock[2]}% {profit:{' '}^{10}}")
-        total += profit
-    print("Portfolio value:", sum(i[1] for i in portfolio))
-    print("Total profit:", "{:.2f}".format(total))
+    desc = ["Name", "Price $", "Profit $"]
+    print(f"{desc[0]:{' '}^{10}} {desc[1]:{' '}^{10}} {desc[2]:{' '}^{10}}")
+    for stock in results[1]:
+        print(f"{stock[0]:{' '}<{10}} {stock[1]:{' '}^{10}} {stock[2]:{' '}^{10}}")
+    valuation = sum(i[1] for i in results[1])
+    print("Portfolio value:", valuation)
+    print("Cumulative profits:", "{:.2f}".format(results[0]))
+    print("Total return:", "{:.2f}".format(valuation + results[0]))
 
 
 def bruteforce(value, stocks, picked=[]):
-    """Try all possible combinations to get the best profit return."""
+    """Try all possible combinations recursively to get the best profit return.
+
+    Args:
+        - value: maximum possible value of a combination
+        - stocks: a list of tuples representing the stocks -> stocks = [(name, price, profit)]
+        - picked: a list of the stocks to pick
+
+    Return:
+        - A tuple -> (total profit, list of picked stocks)
+    """
     if stocks:
-        x, lst_x = bruteforce(value, stocks[1:], picked)
-        # pick first stock anc compare its price with value
+        # recursively call bruteforce by removing the first stock each time
+        x, combi1 = bruteforce(value, stocks[1:], picked)
+
+        # pick the first stock if its price is less or equals to value
         pick = stocks[0]
         if pick[1] <= value:
-            y, lst_y = bruteforce(value - pick[1], stocks[1:], picked + [pick])
+
+            #  add picked stock, update new value and recursively call bruteforce by removing it
+            y, combi2 = bruteforce(value - pick[1], stocks[1:], picked + [pick])
+
+            # return the combination with the best profit
             if x < y:
-                return y, lst_y
-        return x, lst_x
+                return y, combi2
+        return x, combi1
     else:
         return sum(i[2] for i in picked), picked
 
--- presentation/presentation.md
+++ presentation/presentation.md
@@ -6,6 +6,8 @@ theme: gaia
 # Presentation
 
 - Bruteforce algorithm
+  - time complexity is exponential O(n²)
+  - space complexity is linear O(1), no additional storage is needed
 - Optimized algorithm
 
 ---
