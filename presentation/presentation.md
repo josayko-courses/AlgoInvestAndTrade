0a430a7f4ea7ecc15d50d4b947c8ccac74a728a8
Jonny Saykosy <josayko@pm.me>
2022-01-02 15:04:16 +0100

    update calculate_profit

--- presentation/presentation.md
+++ presentation/presentation.md
@@ -7,7 +7,8 @@ theme: gaia
 
 - Bruteforce algorithm
   - time complexity is exponential O(n²)
-  - space complexity is linear O(1), no additional storage is needed
+  - space complexity is linear O(n), memory is allocated for each solution
 - Optimized algorithm
+  - time complexity is O(n \* x), x is the numbers of elements
 
 ---
--- utils/calculate_profit.py
+++ utils/calculate_profit.py
@@ -4,11 +4,11 @@
 
 
 def calculate_profit(results):
-    desc = ["Name", "Price $", "Profit $"]
+    desc = ["Name", "Price €", "Profit €"]
     print(f"{desc[0]:{' '}^{10}} {desc[1]:{' '}^{10}} {desc[2]:{' '}^{10}}")
     for stock in results[1]:
         print(f"{stock[0]:{' '}<{10}} {stock[1]:{' '}^{10}} {stock[2]:{' '}^{10}}")
     valuation = sum(i[1] for i in results[1])
-    print("Portfolio value:", valuation)
-    print("Cumulative profits:", "{:.2f}".format(results[0]))
-    print("Total return:", "{:.2f}".format(valuation + results[0]))
+    print("Portfolio value:", valuation, "€")
+    print("Cumulative profits:", "{:.2f}".format(results[0]), "€")
+    print("Total return:", "{:.2f}".format(valuation + results[0]), "€")
